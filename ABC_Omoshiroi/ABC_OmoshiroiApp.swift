e3282823894424462e8e355a2872d8c1a7d44a85
luzhihao <luzhihao21@gmail.com>
2025-12-23 01:08:03 +0900

    Initial Commit

---
+++ ABC_Omoshiroi/ABC_OmoshiroiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABC_OmoshiroiApp.swift
+//  ABC_Omoshiroi
+//
+//  Created by Michael on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ABC_OmoshiroiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
